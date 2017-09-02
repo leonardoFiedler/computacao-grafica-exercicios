a90ccc486e9cb3a0b982b0f1da8b8b3e276b9473
Flavio Losada <flavio_gs96@hotmail.com>
2017-09-01 21:49:45 -0300

    questao 7

--- ComputacaoGrafica/Exercicio7/Exercicio7.cpp
+++ ComputacaoGrafica/Exercicio7/Exercicio7.cpp
@@ -4,9 +4,73 @@
 GLint gJanelaPrincipal = 0;
 GLint janelaLargura    = 400;
 GLint janelaAltura     = 400;
-int qtdClicked = 0;
-GLenum type = GL_POINTS;
 
+void desenha_circulo(GLfloat posX, GLfloat posY, GLfloat raio, int iQtd)
+{
+	glColor3f(0.0, 0.0, 0.0);
+
+	glLineWidth(2.0f);
+	glBegin(GL_LINE_LOOP);
+	for (int ii = 0; ii < iQtd; ii++)
+	{
+		float theta = 2.0f * 3.1415926f * float(ii) / float(iQtd);
+
+		float x = raio * cosf(theta);
+		float y = raio * sinf(theta);
+
+		glVertex2f(x + posX, y + posY);
+
+	}
+	glEnd();
+}
+
+void desenha_ponto(GLfloat posX, GLfloat posY)
+{
+	glColor3f(0.0, 0.0, 0.0);
+
+	glPointSize(2.0f);
+	glBegin(GL_POINTS);
+		glVertex2d(posX, posY);
+	glEnd();
+}
+
+void desenha_quadrado(GLfloat posX, GLfloat posY)
+{
+	glColor3f(0.8, 0.5, 0.7);
+
+	glLineWidth(2.0f);
+	double pi = 3.14159265358979;
+	glBegin(GL_LINE_LOOP);		
+		double theta = pi * (45) / 200;
+
+		double x = 200 * cos(theta);
+		double y = 200 * sin(theta);
+
+		glVertex2d(x + posX, y + posY);	
+			
+		theta = pi * (-45) / 200;
+
+		x = -200 * cos(theta);
+		y = -200 * sin(theta);
+
+		glVertex2d(x + posX, y + posY);
+
+		theta = pi * (45) / 200;
+
+		x = -200 * cos(theta);
+		y = -200 * sin(theta);
+
+		glVertex2d(x + posX, y + posY);	
+
+		theta = pi * (-45) / 200;
+
+		x = 200 * cos(theta);
+		y = 200 * sin(theta);
+
+		glVertex2d(x + posX, y + posY);
+		
+	glEnd();
+}
 
 void exercicio7()
 {
@@ -19,18 +83,15 @@ void exercicio7()
 
     SRU();
 
-	//implementation
-	glColor3f(0.0, 0.0, 0.0);
-	glPointSize(4.0f);
-	glLineWidth(4.0f);
-	
-	//Sua implementacao aqui
-	glBegin(GL_POINTS);
-
-	glEnd();
+	desenha_circulo(0, 0, 200, 100);
+	desenha_circulo(0, 0, 50, 30);
+	desenha_ponto(0, 0);
+	desenha_quadrado(0, 0);
 
 	glutSwapBuffers();
 }
+
+
 
 void keyboardFunc(UCHAR key, int x, int y)
 {
@@ -60,11 +121,11 @@ void keyboardFunc(UCHAR key, int x, int y)
 
 void mouseFunc(int iButton, int iState, int x, int y)
 {
-	if (iState == GLUT_DOWN && iButton == GLUT_LEFT_BUTTON) 
+	/*if (iState == GLUT_DOWN && iButton == GLUT_LEFT_BUTTON) 
 	{
 		
 
-	}
+	}*/
 	glutPostRedisplay();
 }
 
