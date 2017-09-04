666b84e2061cb51d683dbf37c58b5da48dd93fef
jtomelin <jader.tomelin@gmail.com>
2017-09-04 00:21:09 -0300

    tocando brasa na treta do Flávio

--- ComputacaoGrafica/Exercicio6/Exercicio6.cpp
+++ ComputacaoGrafica/Exercicio6/Exercicio6.cpp
@@ -6,35 +6,19 @@ GLint janelaLargura    = 400;
 GLint janelaAltura     = 400;
 GLenum type = GL_POINTS;
 CArray<CPoint> ListaPontos;
+CPoint pontoSelecionado;
 
 int xAnterior = 0;
 int yAnterior = 0;
 bool bMoveu = false;
 
-CArray<double>& SPLINE_Inter(CPoint A, CPoint B, GLint t, GLboolean desenha, GLint qtdPontos)
+void calculaSpline(GLdouble t, GLdouble& x, GLdouble& y)
 {
-    CArray<double> aPontos;
-    aPontos.SetSize(2);
-    aPontos[0] = A.x + (B.x - A.x) * t / qtdPontos;
-    aPontos[1] = A.y + (B.y - A.y) * t / qtdPontos;
+	double diff = 1.0 - t;
 
-    if (desenha)
-    {
-        glColor3f(0.0f, 0.0f, 1.0f);
-        glRectd(aPontos[0] - 2, aPontos[1] -2, 4, 4);
-    }
-
-    return aPontos;
+	x = pow(diff, 3) * ListaPontos[3].x + 3 * t * pow(diff, 2) * ListaPontos[2].x + 3 * pow(t, 2) * diff * ListaPontos[1].x + pow(t, 3) * ListaPontos[0].x;
+	y = pow(diff, 3) * ListaPontos[3].y + 3 * t * pow(diff, 2) * ListaPontos[2].y + 3 * pow(t, 2) * diff * ListaPontos[1].y + pow(t, 3) * ListaPontos[0].y;
 }
-
-void SPLINE_DESENHA(GLint qtdPontos)
-{
-    if (bMoveu)
-    {
-        
-    }
-}
-
 
 void exercicio6()
 {
@@ -47,17 +31,36 @@ void exercicio6()
 
     SRU();
 
-	glColor3f(0.0, 0.0, 0.0);
-	glPointSize(4.0f);
-	//glLineWidth(4.0f);
+	glColor3f(0.0f, 1.0f, 1.0f);
+	glLineWidth(3.0f);
+
+	//desenhar o poliedro
+	glBegin(GL_LINE_STRIP);
+	for (INT_PTR iIndex = 0; iIndex < ListaPontos.GetSize(); iIndex++)
+        glVertex2f(ListaPontos[iIndex].x, ListaPontos[iIndex].y);
+	glEnd();
+
+	//Desenhar o ponto selecionado
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glPointSize(8.0f);
 	
 	glBegin(GL_POINTS);
+	glVertex2f(pontoSelecionado.x, pontoSelecionado.y);
+	glEnd();
 
-    for (INT_PTR iIndex = 0; iIndex < ListaPontos.GetSize(); iIndex++)
-    {
-        glVertex2f(ListaPontos[iIndex].x, ListaPontos[iIndex].y);
-    }
-    
+	//desenhar a Spline
+	glColor3f(1.0f, 1.0f, 0.0f);
+	glLineWidth(2.0f);
+	glBegin(GL_LINE_STRIP);
+	
+	for (int t = 0; t <= 40; t++){
+		GLdouble xRet = 0;
+		GLdouble yRet = 0;
+
+		calculaSpline(t * 0.025, xRet, yRet);
+		glVertex2f(xRet, yRet);
+	}
+	
 	glEnd();
 
 	glutSwapBuffers();
@@ -68,25 +71,25 @@ void keyboardFunc(UCHAR key, int x, int y)
 	switch (key)
 	{
 	case '1':
-
+		pontoSelecionado = ListaPontos[3];
 		break;
 
 	case '2':
-
+		pontoSelecionado = ListaPontos[2];
 		break;
 
 	case '3':
-
+		pontoSelecionado = ListaPontos[1];
 		break;
 
 	case '4':
-
+		pontoSelecionado = ListaPontos[0];
 		break;
 	default:
 		break;
 	}
 
-	exercicio6();
+	glutPostRedisplay();
 }
 
 void mouseFunc(int iButton, int iState, int x, int y)
@@ -136,22 +139,25 @@ int main(int argc, TCHAR* argv[], TCHAR* envp[])
 
     CPoint p1, p2, p3, p4;
 
-    p1.x = -150;
-    p1.y =  150;
+    p1.x =  100;
+    p1.y = -100;
 
-    p2.x = -150;
-    p2.y = -150;
+    p2.x =  100;
+    p2.y =  100;
 
-    p3.x =  150;
-    p3.y = -150;
+    p3.x = -100;
+    p3.y =  100;
 
-    p4.x = 125;
-    p4.y = 125;
+    p4.x = -100;
+    p4.y = -100;
 
     ListaPontos[0] = p1;
     ListaPontos[1] = p2;
     ListaPontos[2] = p3;
     ListaPontos[3] = p4;
+
+	//definindo como ponto inicial o p4
+	pontoSelecionado = p4;
 
 	glutInit(&argc, (char **)argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
